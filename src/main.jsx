615fd635ab52c034b7517dad2c1b036624009095
ThiagoVanucci <thiago.dev24@gmail.com>
2025-10-01 14:14:02 -0300

    Ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-<<<<<<< HEAD
 import Home from '../pages/Home.jsx';
 import SobreNos from '../pages/SobreNos.jsx';
 import Contato from '../pages/Contato.jsx';
-=======
 import Home from "../pages/Home.jsx";
 import SobreNos from "../pages/SobreNos.jsx";
 import Servicos from "../pages/Servicos.jsx";
->>>>>>> origin/Tiago
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -22,11 +19,8 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/sobrenos" element={<SobreNos />} />
-<<<<<<< HEAD
         <Route path="/contato" element={<Contato />} />
-=======
         <Route path="/servicos" element={<Servicos />} />
->>>>>>> origin/Tiago
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
