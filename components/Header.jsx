b256306b717932ee7e575fc6ce302fd8dd124de4
ThiagoVanucci <thiago.dev24@gmail.com>
2025-10-01 11:45:51 -0300

    Header Modificado

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,44 @@
 import { Link, NavLink } from "react-router"
+import Logo from "../src/img/Logo.png"
+import "./Header.css" // Arquivo de estilos extras
 
 const Header = () => {
-
   return (
     <header>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">IOTEC</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-      data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav mx-auto">
-        <a class="nav-link" href="/">Home</a>
-        <a class="nav-link" href="/sobrenos">Sobre Nós</a>
-        <a class="nav-link" href="/servicos">Serviços</a>
-        <a class="nav-link" href="/contatos">Contatos</a>
-      </div>
-    </div>
-  </div>
-  <div>
-    <img src=""></img>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
+        <div className="container-fluid">
 
+          {/* Logo */}
+          <Link className="navbar-brand ms-4" to="/">
+            <img 
+              src={Logo} 
+              alt="Logo IOTEC" 
+              style={{ height: "50px", width: "auto" }} 
+            />
+          </Link>
+
+          <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNavAltMarkup" 
+            aria-controls="navbarNavAltMarkup" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav mx-auto gap-4">
+              <NavLink className="nav-link custom-link" to="/">Home</NavLink>
+              <NavLink className="nav-link custom-link" to="/sobrenos">Sobre Nós</NavLink>
+              <NavLink className="nav-link custom-link" to="/servicos">Serviços</NavLink>
+              <NavLink className="nav-link custom-link" to="/contato">Contato</NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
     </header>
   )
 }
