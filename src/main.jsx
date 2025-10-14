15697b773da2339a2370b7fa7238806c8e075b60
Tiago Pereira <tiagopereiraduarte8@gmail.com>
2025-10-14 16:23:28 -0300

    Add TeamMembers component and update About page with team details and mission statement

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Home from '../pages/Home.jsx';
-=======
->>>>>>> origin/Deverick
-import SobreNos from '../pages/SobreNos.jsx';
-import Contato from '../pages/Contato.jsx';
-=======
-import Home from "../pages/Home.jsx";
+
 import SobreNos from "../pages/SobreNos.jsx";
-import Servicos from "../pages/Servicos.jsx";
->>>>>>> origin/Tiago
+import Contato from "../pages/Contato.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -24,11 +16,7 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/sobrenos" element={<SobreNos />} />
-<<<<<<< HEAD
         <Route path="/contato" element={<Contato />} />
-=======
-        <Route path="/servicos" element={<Servicos />} />
->>>>>>> origin/Tiago
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
